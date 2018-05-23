162fc112ecf92005a150f7244d40aa8bfea146ec
Michael Balszun <michael.balszun@tum.de>
2018-05-23 17:35:04 +0200

    add missing inline specifier and run clang format

--- units/fmt.h
+++ units/fmt.h
@@ -1,54 +1,61 @@
 #pragma once
 
-#include <iostream>
 #include "./units.h"
+#include <iostream>
 
 namespace units {
 
-	template<int k, int m, int s>
-	struct FormattedUnit {
-		Unit<k, m, s> u;
-	};
+template<int k, int m, int s>
+struct FormattedUnit {
+	Unit<k, m, s> u;
+};
 
-	struct FormattedAngle {
-		UAngle u;
-	};
+struct FormattedAngle {
+	UAngle u;
+};
 
-	template<int k, int m, int s>
-	FormattedUnit<k, m, s> sformat(Unit<k, m, s> value) {
-		return { value };
-	}
+template<int k, int m, int s>
+FormattedUnit<k, m, s> sformat( Unit<k, m, s> value )
+{
+	return {value};
+}
 
-	FormattedAngle sformat(UAngle value) {
-		return { value };
-	}
+inline FormattedAngle sformat( UAngle value )
+{
+	return {value};
+}
 
-	template<int k, int m, int s>
-	std::ostream& operator<<(std::ostream& out, const FormattedUnit<k, m, s>& u) {
-		out << u.u.value << "";
+template<int k, int m, int s>
+std::ostream& operator<<( std::ostream& out, const FormattedUnit<k, m, s>& u )
+{
+	out << u.u.value << "";
+	// clang-format off
 		if constexpr (k == 1) { out << "kg" << " "; } else if constexpr (k != 0) { out << "kg^" << k << " "; }
 		if constexpr (m == 1) { out << "m"  << " "; } else if constexpr (m != 0) { out << "m^" << m << " "; }
 		if constexpr (s == 1) { out << "s"  << " "; } else if constexpr (s != 0) { out << "s^" << s << " "; }
-		std::cout << "";
+	// clang-format on
+	std::cout << "";
 
-		return out;
-	}
-
-	template<int k, int m, int s>
-	std::ostream& operator<<(std::ostream& out, const Unit<k, m, s> u) {
-		out << u.value;
-		return out;
-	}
-
-	std::ostream& operator<<(std::ostream& out, const UAngle u) {
-		out << u.value;
-		return out;
-	}
-
-	std::ostream& operator<<(std::ostream& out, const FormattedAngle u) {
-		out << u.u;
-		return out;
-	}
-
-
+	return out;
 }
+
+template<int k, int m, int s>
+std::ostream& operator<<( std::ostream& out, const Unit<k, m, s> u )
+{
+	out << u.value;
+	return out;
+}
+
+inline std::ostream& operator<<( std::ostream& out, const UAngle u )
+{
+	out << u.value;
+	return out;
+}
+
+inline std::ostream& operator<<( std::ostream& out, const FormattedAngle u )
+{
+	out << u.u;
+	return out;
+}
+
+} // namespace units
